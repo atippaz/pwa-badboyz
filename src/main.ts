dcb07470cc5dd3781a61dcebac192fcdd1adfb12
atippaz <nasakun13201@gmail.com>
2023-10-31 09:31:20 -0700

    update

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,30 @@ import { createApp } from 'vue'
 import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./service-worker.js').then((registration) => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        });
-    });
-}
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', () => {
+//         navigator.serviceWorker.register('./service-worker.js').then((registration) => {
+//             console.log('Service Worker registered with scope:', registration.scope);
+//         });
+//     });
+// }
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+        try {
+            const registration = await navigator.serviceWorker.register("./service-worker.js");
+            if (registration.installing) {
+                console.log("Service worker installing");
+            } else if (registration.waiting) {
+                console.log("Service worker installed");
+            } else if (registration.active) {
+                console.log("Service worker active");
+            }
+        } catch (error) {
+            console.error(`Registration failed with ${error}`);
+        }
+    }
+};
+registerServiceWorker()
 registerPlugins(app)
 
 app.mount('#app')
