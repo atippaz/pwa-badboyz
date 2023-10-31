6bcc91d1236f8e28da90132194a2ec3f61f39d48
atippaz <nasakun13201@gmail.com>
2023-10-31 09:21:40 -0700

    ียกฟะำ

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,53 +1,53 @@
-// service-worker.js
-const cacheName = 'my-pwa-cache-v1';
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(cacheName).then((cache) => {
-            return cache.addAll([
-                '/',
-                // '/index.html',
-                '/assets',
-                // '/styles.css',
-                // เพิ่ม URL อื่น ๆ ที่คุณต้องการแคช
-            ]);
-        })
-    );
-});
+// // service-worker.js
+// const cacheName = 'workbox-precache-v2';
+// // self.addEventListener('install', (event) => {
+// //     event.waitUntil(
+// //         caches.open(cacheName).then((cache) => {
+// //             return cache.addAll([
+// //                 '/',
+// //                 // '/index.html',
+// //                 '/assets',
+// //                 // '/styles.css',
+// //                 // เพิ่ม URL อื่น ๆ ที่คุณต้องการแคช
+// //             ]);
+// //         })
+// //     );
+// // });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((name) => {
-                    if (name !== cacheName) {
-                        return caches.delete(name);
-                    }
-                })
-            );
-        })
-    );
-});
+// self.addEventListener('activate', (event) => {
+//     event.waitUntil(
+//         caches.keys().then((cacheNames) => {
+//             return Promise.all(
+//                 cacheNames.map((name) => {
+//                     if (name !== cacheName) {
+//                         return caches.delete(name);
+//                     }
+//                 })
+//             );
+//         })
+//     );
+// });
 
-self.addEventListener('fetch', (event) => {
-    if (event.request.mode === 'navigate') {
-        event.respondWith(
-            fetch(event.request)
-                .then((response) => {
-                    return caches.open(cacheName)
-                        .then((cache) => {
-                            cache.put(event.request, response.clone());
-                            return response;
-                        });
-                })
-                .catch(() => {
-                    return caches.match(event.request);
-                })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request).then((response) => {
-                return response || fetch(event.request);
-            })
-        );
-    }
-});
+// self.addEventListener('fetch', (event) => {
+//     if (event.request.mode === 'navigate') {
+//         event.respondWith(
+//             fetch(event.request)
+//                 .then((response) => {
+//                     return caches.open(cacheName)
+//                         .then((cache) => {
+//                             cache.put(event.request, response.clone());
+//                             return response;
+//                         });
+//                 })
+//                 .catch(() => {
+//                     return caches.match(event.request);
+//                 })
+//         );
+//     } else {
+//         event.respondWith(
+//             caches.match(event.request).then((response) => {
+//                 return response || fetch(event.request);
+//             })
+//         );
+//     }
+// });
