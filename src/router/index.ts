3053281642a09a31faae4704eaf6da3fe317336c
Atip Nasakun <71032885+atippaz@users.noreply.github.com>
2023-11-01 09:13:54 +0700

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,13 +15,7 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import('@/views/Home.vue'),
-      },{
-        path: 'index.html',
-        name: 'HomePageindex',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import('@/views/Home.vue'),
+alias:'index.html',
       },
       {
         path: 'team-view/:roomId',
