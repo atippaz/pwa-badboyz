f29fb1bb4b3fb25ab6eb6ceabc75274d70240b3b
Atip Nasakun <71032885+atippaz@users.noreply.github.com>
2023-11-01 08:44:32 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,2 @@
 module.exports = {
-    pwa: {
-        name: 'BadBoyz',
-        themecolor: '#000000',
-        mstilecolor: '#000000',
-        applemobilewebappcapable: 'yes',
-        applemobilewebappstatusbarstyle: 'black',
-        workboxoptions: {
-            navigatefallback: './index.html',
-        },
-    }
 };
