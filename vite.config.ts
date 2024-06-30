67ba79aa32638490372e7a64b95efc0d1b033c08
atippaz <nasakun13201@gmail.com>
2024-06-30 09:32:15 +0700

    disable pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,81 +1,75 @@
 // Plugins
-import vue from '@vitejs/plugin-vue'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-import ViteFonts from 'unplugin-fonts/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import vue from "@vitejs/plugin-vue";
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+import ViteFonts from "unplugin-fonts/vite";
+import { VitePWA } from "vite-plugin-pwa";
 // Utilities
-import { defineConfig } from 'vite'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: "./",
 
   plugins: [
     vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
       styles: {
-        configFile: 'src/styles/settings.scss',
+        configFile: "src/styles/settings.scss",
       },
     }),
     ViteFonts({
       google: {
-        families: [{
-          name: 'Roboto',
-          styles: 'wght@100;300;400;500;700;900',
-        }],
-      },
-    }),
-    VitePWA({
-      devOptions: {
-        enabled: true,
-      },
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      workbox: {
-        // กำหนดค่าการตั้งค่าของ Service Worker ตามความต้องการ
-        globPatterns: ['**/*.{js,css,html,png}'],
-      },
-      manifest: {
-        // กำหนดค่า manifest ของ PWA ที่คุณต้องการ
-        name: 'BadBoyz',
-        description: 'my pwa',
-        short_name: 'BadBoyz',
-        start_url: "./index.html",
-        scope: "./index.html",
-        display: 'standalone',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        icons: [
+        families: [
           {
-            src: './icon.ico',
-            sizes: '128x128',
-            type: 'image/x-icon',
+            name: "Roboto",
+            styles: "wght@100;300;400;500;700;900",
           },
         ],
       },
     }),
+    // VitePWA({
+    //   devOptions: {
+    //     enabled: true,
+    //   },
+    //   registerType: "autoUpdate",
+    //   injectRegister: "auto",
+    //   workbox: {
+    //     // กำหนดค่าการตั้งค่าของ Service Worker ตามความต้องการ
+    //     globPatterns: ["**/*.{js,css,html,png}"],
+    //   },
+    //   manifest: {
+    //     // กำหนดค่า manifest ของ PWA ที่คุณต้องการ
+    //     name: "BadBoyz",
+    //     description: "my pwa",
+    //     short_name: "BadBoyz",
+    //     start_url: "./index.html",
+    //     scope: "./index.html",
+    //     display: "standalone",
+    //     background_color: "#ffffff",
+    //     theme_color: "#000000",
+    //     icons: [
+    //       {
+    //         src: "./icon.ico",
+    //         sizes: "128x128",
+    //         type: "image/x-icon",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
-  define: { 'process.env': {} },
+  define: { "process.env": {} },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ],
+    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
   server: {
     port: 3000,
   },
-})
+});
