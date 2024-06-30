5b7c53ae78163680ad7a86b9cc35416fa2cd0f12
atippaz <nasakun13201@gmail.com>
2024-06-30 09:14:42 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -24,17 +24,19 @@ const app = createApp(App);
 try {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
-      navigator.serviceWorker.register("/service-worker.js").then(
-        function (registration) {
-          console.log(
-            "ServiceWorker registration successful with scope: ",
-            registration.scope
-          );
-        },
-        function (err) {
-          console.log("ServiceWorker registration failed: ", err);
-        }
-      );
+      navigator.serviceWorker
+        .register(`${window.location.origin}/service-worker.js`)
+        .then(
+          function (registration) {
+            console.log(
+              "ServiceWorker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("ServiceWorker registration failed: ", err);
+          }
+        );
     });
   }
 } catch (ex) {
