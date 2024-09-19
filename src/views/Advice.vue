e52d37a59627cbe54bf0710f7eef463a19563ff3
atippaz <nasakun13201@gmail.com>
2024-09-19 22:14:21 +0700

    rename html page

--- src/views/Advice.vue
+++ src/views/Advice.vue
@@ -1,18 +1,57 @@
 <template>
   <main>
-    <section>
+    <section class="pt-16 d-flex justify-center">
       <header>
-        <h1>เริ่มใช้งาน Rackoo</h1>
+        <h1>เริ่มใช้งานเเรคคู่</h1>
       </header>
     </section>
-    <section>logo</section>
-    <section>Rackoo</section>
-    <section>ให้เเรคคู่ช่วยจับคู่ตีเเบดให้สิ!!</section>
+    <section class="d-flex justify-center">
+      <img src="/public/rackoo-icon.png" alt="rackoo" width="100" />
+    </section>
+    <section class="d-flex justify-center">Rackoo</section>
+
+    <section class="bg-purple-lighten-2 pa-8">
+      <div>ให้เเรคคู่ช่วยจับคู่ตีเเบดให้สิ!!</div>
+      <div>สร้างห้องของคุณเเละเพื่อนๆของคุณ หรือ เลือกห้องของคุณเอง</div>
+    </section>
+    <section class="bg-purple-lighten-2 pa-8">
+      <div>ถ้าสร้างบัญชีกับเราไว้ เราช่วยจับคู่ล่วงหน้าให้ด้วยนะ!!</div>
+      <div>สามารถสร้างลิสต์คนตีประจำได้</div>
+    </section>
     <section class="bg-purple-lighten-2 pa-8">
       <main>
         <p>ใส่รายชื่อคนในทีม</p>
         <p>สามารถใส่ได้ทั้งมีตัวเลขเเละไม่มีตัวเลข</p>
       </main>
     </section>
+    <section class="bg-purple-lighten-2 pa-8">
+      <main>
+        <p>ปรับการตั้งค่าเพิ่มเติม</p>
+        <p>สามารถเลือกการต้งค่าเพิ่มเติมได้</p>
+      </main>
+    </section>
+    <section class="bg-purple-lighten-2 pa-8">
+      <main>
+        <p>หากต้องการจับคู่ไม่ซ้ำกันหลายๆรอบก็ทำได้</p>
+        <p>เลือกปุ่มการจับคู๋ไม่ซ้ำกัน</p>
+      </main>
+    </section>
+    <section class="bg-purple-lighten-2 pa-8">
+      <main>
+        <p>จับคู่สำเร็จเเชร์ลิ้งไปให้เพื่อนๆเข้ามาดูห้องได้่เลย!!</p>
+        <p>หากเลือกจับคู๋เเบบไม่ซ้ำ สามารถปัดจอซ้ายขวา เพื่อดูเซ็๋ตอื่นๆได้</p>
+      </main>
+    </section>
+    <section class="bg-purple-lighten-2 pa-8">
+      <main>
+        <p>จับคู่สำเร็จเเชร์ลิ้งไปให้เพื่อนๆเข้ามาดูห้องได้่เลย!!</p>
+        <p>หากเลือกจับคู๋เเบบไม่ซ้ำ สามารถปัดจอซ้ายขวา เพื่อดูเซ็๋ตอื่นๆได้</p>
+      </main>
+    </section>
   </main>
 </template>
+<style scoped>
+.header {
+  font-size: ;
+}
+</style>
