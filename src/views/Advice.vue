ef7f3ded6ac683c89c0a75cad971012f1b4cf8de
atippaz <nasakun13201@gmail.com>
2024-09-19 22:21:25 +0700

    add page

--- src/views/Advice.vue
+++ src/views/Advice.vue
@@ -10,41 +10,46 @@
     </section>
     <section class="d-flex justify-center">Rackoo</section>
 
-    <section class="bg-purple-lighten-2 pa-8">
+    <section class="bg-deep-purple-lighten-4 pa-8">
       <div>ให้เเรคคู่ช่วยจับคู่ตีเเบดให้สิ!!</div>
       <div>สร้างห้องของคุณเเละเพื่อนๆของคุณ หรือ เลือกห้องของคุณเอง</div>
+      <img src="/public/createroom.png" width="300" alt="" />
     </section>
-    <section class="bg-purple-lighten-2 pa-8">
+    <section class="bg-deep-purple-lighten-4 pa-8">
       <div>ถ้าสร้างบัญชีกับเราไว้ เราช่วยจับคู่ล่วงหน้าให้ด้วยนะ!!</div>
       <div>สามารถสร้างลิสต์คนตีประจำได้</div>
     </section>
-    <section class="bg-purple-lighten-2 pa-8">
+    <section class="bg-deep-purple-lighten-3 pa-8">
       <main>
         <p>ใส่รายชื่อคนในทีม</p>
         <p>สามารถใส่ได้ทั้งมีตัวเลขเเละไม่มีตัวเลข</p>
+        <img src="/public/config.png" width="300" alt="" />
       </main>
     </section>
-    <section class="bg-purple-lighten-2 pa-8">
+    <section class="bg-deep-purple-lighten-4 pa-8">
       <main>
         <p>ปรับการตั้งค่าเพิ่มเติม</p>
         <p>สามารถเลือกการต้งค่าเพิ่มเติมได้</p>
+        <img src="/public/config.png" width="300" alt="" />
       </main>
     </section>
-    <section class="bg-purple-lighten-2 pa-8">
+    <section class="bg-deep-purple-lighten-3 pa-8">
       <main>
         <p>หากต้องการจับคู่ไม่ซ้ำกันหลายๆรอบก็ทำได้</p>
         <p>เลือกปุ่มการจับคู๋ไม่ซ้ำกัน</p>
+        <img src="/public/config.png" width="300" alt="" />
       </main>
     </section>
-    <section class="bg-purple-lighten-2 pa-8">
+    <section class="bg-deep-purple-lighten-4 pa-8">
       <main>
         <p>จับคู่สำเร็จเเชร์ลิ้งไปให้เพื่อนๆเข้ามาดูห้องได้่เลย!!</p>
         <p>หากเลือกจับคู๋เเบบไม่ซ้ำ สามารถปัดจอซ้ายขวา เพื่อดูเซ็๋ตอื่นๆได้</p>
+        <img src="/public/preview.png" width="300" alt="" />
       </main>
     </section>
-    <section class="bg-purple-lighten-2 pa-8">
+    <section class="bg-deep-purple-lighten-3 pa-8">
       <main>
-        <p>จับคู่สำเร็จเเชร์ลิ้งไปให้เพื่อนๆเข้ามาดูห้องได้่เลย!!</p>
+        <p>rackoo ขอให้คุณสนุกนะ</p>
         <p>หากเลือกจับคู๋เเบบไม่ซ้ำ สามารถปัดจอซ้ายขวา เพื่อดูเซ็๋ตอื่นๆได้</p>
       </main>
     </section>
@@ -52,6 +57,5 @@
 </template>
 <style scoped>
 .header {
-  font-size: ;
 }
 </style>
